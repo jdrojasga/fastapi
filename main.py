7ad6eab740ec1e86564fb9f8aa37b7f9165d52a0
jdrojasga <jdrojasga@unal.edu.co>
2025-05-15 19:48:00 -0500

    feat: runing api with the cli

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 """Main module for the FastAPI application."""
 
+from typing import Union
 
-def main():
-    """Main function to run the FastAPI application."""
-    print("Hello from fastapi!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
+def run():
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
