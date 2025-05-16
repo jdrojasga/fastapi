f2b1cd0d706c1aff7478b0d22a2c98895042728a
jdrojasga <jdrojasga@unal.edu.co>
2025-05-15 19:32:54 -0500

    feat: initialize projectu using uv

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Main module for the FastAPI application."""
+
+
+def main():
+    """Main function to run the FastAPI application."""
+    print("Hello from fastapi!")
+
+
+if __name__ == "__main__":
+    main()
